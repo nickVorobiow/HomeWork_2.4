9b79bfe1e2fa244d962032c35f6747ab9efd24d9
nickVorobiow <vorobyev.nikitos@gmail.com>
2023-01-17 02:52:16 +0300

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,44 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Auto auto1 = new Auto();
+        auto1.setBrand("Lada");
+        auto1.setModel("Granta");
+        auto1.setYear("2015");
+        auto1.setCountry("Russia");
+        auto1.setColor("yellow");
+        auto1.setEngineVolume("1,7");
 
+        Auto auto2 = new Auto();
+        auto2.setBrand("Audi");
+        auto2.setModel("A8 50 L TDI quattro");
+        auto2.setYear("2020");
+        auto2.setCountry("Germany");
+        auto2.setColor("black");
+        auto2.setEngineVolume("3,0");
+
+        Auto auto3 = new Auto();
+        auto2.setBrand("BMW");
+        auto2.setModel("Z8");
+        auto3.setYear("2021");
+        auto2.setCountry("Germany");
+        auto2.setColor("black");
+        auto2.setEngineVolume("3,0");
+
+        Auto auto4 = new Auto();
+        auto2.setBrand("Kia");
+        auto2.setModel("Sportage 4th generation");
+        auto4.setYear("2018");
+        auto2.setCountry("Southern Korea");
+        auto2.setColor("red");
+        auto2.setEngineVolume("2,4");
+
+        Auto auto5 = new Auto();
+        auto2.setBrand("Hyundai");
+        auto2.setModel("Avante");
+        auto5.setYear("2016");
+        auto2.setCountry("Southern Korea");
+        auto2.setColor("orange");
+        auto2.setEngineVolume("1,6");
     }
 }
