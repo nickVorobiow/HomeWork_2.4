d995cad650872ad87c0103e1c2fff62cb22d0dbc
nickVorobiow <vorobyev.nikitos@gmail.com>
2023-01-17 02:24:50 +0300

    initial commit

---
+++ src/com/company/Auto.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Auto {
+    private String brand;
+    private String model;
+    private String engineVolume;
+    private String color;
+    private String year;
+    private String country;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(String engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume='" + engineVolume + '\'' +
+                ", color='" + color + '\'' +
+                ", year='" + year + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
