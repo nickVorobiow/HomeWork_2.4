cc7b7180294ea22365e10a66b7e3efd95d573a1a
nickVorobiow <vorobyev.nikitos@gmail.com>
2023-01-17 03:39:13 +0300

    do 3nd task

--- src/com/company/Auto.java
+++ src/com/company/Auto.java
@@ -9,12 +9,23 @@ public class Auto {
     private String country;
 
     public Auto(String brand, String model, String engineVolume, String color, String year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (model == "") {this.model = "default";}
+        else {this.model = model;}
+
+        if (brand == "") {this.brand = "default";}
+        else {this.brand = brand;}
+
+        if (country == "") {this.country = "default";}
+        else {this.country = country;}
+
+        if (Double.parseDouble(engineVolume) <= 0) {this.engineVolume = "1.5";}
+        else {this.engineVolume = engineVolume;}
+
+        if (color == "") {this.color = "white";}
+        else {this.color = color;}
+
+        if (Integer.parseInt(year) <= 0) {this.year = "2000";}
+        else {this.year = year;}
     }
 
     public String getBrand() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Auto auto1 = new Auto("Lada", "Granta", "1,6", "yellow",
-                "2015", "Russia");
-        Auto auto2 = new Auto("Audi", "A8 50 L TDI quattro", "3,0", "black",
+        Auto auto1 = new Auto("Lada", "Granta", "1.6", "yellow",
+                "0", "Russia");
+        Auto auto2 = new Auto("Audi", "A8 50 L TDI quattro", "3.0", "black",
                 "2020", "Germany");
-        Auto auto3 = new Auto("BMW", "Z8", "3,0", "black",
-                "2021", "Germany");
-        Auto auto4 = new Auto("Kia", "Sportage 4th generation", "2,4", "red",
+        Auto auto3 = new Auto("BMW", "Z8", "3.0", "black",
+                "0", "Germany");
+        Auto auto4 = new Auto("Kia", "Sportage 4th generation", "2.4", "red",
                 "2018", "Southern Korea");
-        Auto auto5 = new Auto("Huyndai", "Avante", "1,6", "orange",
+        Auto auto5 = new Auto("Huyndai", "Avante", "0", "orange",
                 "2016", "Southern Korea");
         System.out.println(auto1);
         System.out.println(auto2);
